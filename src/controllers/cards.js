0164744cb81a6a36f69df415c9674ec9cdbefeda
jestir1234 <jestir1234@gmail.com>
2018-05-23 00:12:24 -0400

    feat($server-side-caching): Cache request data
    Implement server side caching with hapi's native caching via Server.methods

--- src/controllers/cards.js
+++ src/controllers/cards.js
@@ -1,6 +1,6 @@
 import { fetchCards } from "./requestHandler";
 
-export const findCard = async (req, res) => {
-  const cards = await fetchCards(req.query, res);
-  res.send(cards);
+export const findCard = async (query, h) => {
+  const cards = await fetchCards(query, h);
+  return cards;
 };
--- src/index.js
+++ src/index.js
@@ -2,20 +2,52 @@
 const someFunction = () => console.log("cat");
 const someOtherFunction = () => console.log("poop");
 import colors from "colors";
-import express from "express";
+import Hapi from "hapi";
 import { findCard } from "./controllers";
 
-const app = express();
-
-app.get("/notes", (req, res) => {
-  res.json({
-    notes: "This is your potato. Edit this to start saving your notes!"
-  });
+const server = Hapi.server({
+  host: "localhost",
+  port: 8000
 });
 
-app.get("/cards", (req, res) => {
-  findCard(req, res);
+server.route({
+  method: "GET",
+  path: "/notes",
+  handler: (request, h) => {
+    return {
+      notes: "This is your potato. Edit this to start saving your notes!"
+    };
+  }
 });
 
-console.log("Listening on port".blue, "3000".underline.red);
-app.listen(3000);
+server.route({
+  method: "GET",
+  path: "/cards",
+  handler: (request, h) => {
+    return server.methods.findCards(request.query);
+  }
+});
+
+server.method("findCards", findCard, {
+  cache: {
+    expiresIn: 60000,
+    staleIn: 30000,
+    staleTimeout: 10000,
+    generateTimeout: 10000
+  },
+  generateKey: result => {
+    return result.name;
+  }
+});
+
+const start = async () => {
+  try {
+    await server.start();
+  } catch (err) {
+    console.log(err);
+    process.exit(1);
+  }
+  console.log("Server running at:", server.info.uri.underline.red);
+};
+
+start();
